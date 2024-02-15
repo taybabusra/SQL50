9a3ec2cc79fdc7f186623eb2a2de96259628f88c
TAYBA BUSRA <taybabusra@gmail.com>
2024-02-15 21:45:20 +0600

    Update referee.sql

--- Day2/referee.sql
+++ Day2/referee.sql
@@ -4,3 +4,11 @@ SELECT name
 FROM Customer
 Where referee_id <> 2  # NULL value are not defined
 OR referee_id IS NULL; # NULL value checked
+
+
+# ANother method using substitution NULL with -1
+# Write your MySQL query statement below
+SELECT name
+
+FROM Customer
+Where IFNULL(referee_id,-1) <> 2
