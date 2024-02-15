0796b9a5406aabe0b14bd08fb6f4d684353a99fd
TAYBA BUSRA <taybabusra@gmail.com>
2024-02-15 21:50:25 +0600

    Update referee.sql

--- Day2/referee.sql
+++ Day2/referee.sql
@@ -12,3 +12,14 @@ SELECT name
 
 FROM Customer
 Where IFNULL(referee_id,-1) <> 2
+
+
+# ANother method when multiple columns have a NULL value then this method is used
+
+# Write your MySQL query statement below
+SELECT name
+
+FROM Customer
+Where COALESCE(IFNULL(referee_id,-1)) <> 2
+
+
