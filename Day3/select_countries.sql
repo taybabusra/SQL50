3ead9e44e2baa832a7b8bf205c7f72a21a86e79c
TAYBA BUSRA <taybabusra@gmail.com>
2024-02-16 08:44:16 +0600

    Update select_countries.sql

--- Day3/select_countries.sql
+++ Day3/select_countries.sql
@@ -1,1 +1,8 @@
-
+# Write your MySQL query statement below
+ Select name, population, area
+ FROM World
+ Where area >= 3000000  # shorting with respect to area
+ Union  # for two different table join
+ Select name, population, area
+ FROM World
+ Where population >= 25000000  #shorting with respect to population
