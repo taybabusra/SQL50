c074e5a06716c6406c3ee68dd3338c9064972bb6
TAYBA BUSRA <taybabusra@gmail.com>
2024-02-16 08:48:10 +0600

    Update select_countries.sql

--- Day3/select_countries.sql
+++ Day3/select_countries.sql
@@ -1,8 +1,15 @@
 # Write your MySQL query statement below
  Select name, population, area
  FROM World
- Where area >= 3000000  # shorting with respect to area
+ Where area >= 3000000  # shorting concerning area
  Union  # for two different table join
  Select name, population, area
  FROM World
- Where population >= 25000000  #shorting with respect to population
+ Where population >= 25000000  #shorting concerning population
+
+
+# Write your MySQL query statement below
+ Select name, population, area
+ FROM World
+ Where area >= 3000000
+ OR population >= 25000000;
